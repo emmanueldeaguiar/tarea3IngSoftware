71ee5f787ec0f6c5f39ea972586016654ab9ca35
Emmanuel <emmanuel.deaguiar@gmail.com>
2015-01-28 18:48:42 -0430

    Comienzo de proyecto archivos en blancp

---
+++ IngSoftwaretarea3/TDD.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 28, 2015
+
+@author: Emmanuel
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ IngSoftwaretarea3/estacionamiento.py
@@ -0,0 +1,5 @@
+'''
+Created on Jan 28, 2015
+
+@author: Emmanuel
+'''
