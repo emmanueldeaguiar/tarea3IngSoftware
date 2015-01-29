aec05daa903fd0eed2db140db23f9df5b34b1c62
Emmanuel <emmanuel.deaguiar@gmail.com>
2015-01-28 19:50:17 -0430

    Dos casos prueba, Nueva clase Estacionamiento
    Se creo clase Estacionamiento para el almacenamiento de todas las
    reservas.
    Pendiente algoritmo para comprobar la disponibilidad del estacionamiento

--- IngSoftwaretarea3/TDD.py
+++ IngSoftwaretarea3/TDD.py
@@ -12,16 +12,33 @@ class TestReservar(unittest.TestCase):
 
     def testReservarExiste(self):
         reserva = Reserva(6,7)
-        reservar(reserva)
+        est = Estacionamiento(10)
+        est.reservar(reserva)
         
     def testReservarPuesto(self):
         reserva = Reserva(6,7)
-        self.assertEqual(reservar(reserva), True)
+        est = Estacionamiento(10)
+        self.assertEqual(est.reservar(reserva), True)
         
     def testReservarFueraHorario(self):
         reserva = Reserva(3,7)
-        self.assertFalse(reservar(reserva))
+        est = Estacionamiento(10)
+        self.assertFalse(est.reservar(reserva))
         
     def testReservarHorasIntercambiadas(self):
         reserva = Reserva(8,7)
-        self.assertFalse(reservar(reserva))
+        est = Estacionamiento(10)
+        self.assertFalse(est.reservar(reserva))
+        
+    def testAlmacenarReserva(self):
+        reserva = Reserva(7,12)
+        est = Estacionamiento(10)
+        est.reservar(reserva)
+        self.assertEqual(est.reservas[0].getInicio(),7)
+        self.assertEqual(est.reservas[0].getFin(),12)
+        
+    def testComprobarDisponibilidadIntervalo(self):
+        reserva = Reserva(7,12)
+        est = Estacionamiento(10)
+        est.comprobarDisponibilidad(est.reservas)
+        
--- IngSoftwaretarea3/estacionamiento.py
+++ IngSoftwaretarea3/estacionamiento.py
@@ -4,6 +4,7 @@ Created on 27/1/2015
 @author: Daniel Pelayo
          Emanuel De Aguiar
 '''
+from test.test_buffer import cap
 class Reserva(object):
     
 
@@ -14,13 +15,24 @@ class Reserva(object):
         return self.__inicio    
     def getFin(self):
         return self.__fin
+    
+class Estacionamiento(object):
+    
+    def __init__(self,cap):
+        self.capacidad = cap
+        self.reservas = []
+        
+    def comprobarDisponibilidad(self,lista):
+        pass
 
-def reservar(reserva):
-    if (reserva.getInicio()<6) or (reserva.getFin() > 18):
-        print('El estacionamiento solo funciona de 6 a 18')
-        return False
-    elif(reserva.getFin()<reserva.getInicio()):
-        print('La hora de entrada debe ser menor que la hora de salida.')
-        return False
-    else: 
+    def reservar(self,intervalo):
+        if (intervalo.getInicio()<6) or (intervalo.getFin() > 18):
+            print('El estacionamiento solo funciona de 6 a 18')
+            return False
+        elif(intervalo.getFin()<intervalo.getInicio()):
+            print('La hora de entrada debe ser menor que la hora de salida.')
+            return False
+            
+        self.reservas.append(intervalo)
         return True
+        
